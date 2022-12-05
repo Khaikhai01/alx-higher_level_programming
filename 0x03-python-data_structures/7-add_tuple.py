ddcb6efed297ad144475734e05fb066732e17a33
Khaikhai01 <shishchei@gmail.com>
2022-12-05 11:49:11 +0300

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -12,5 +12,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
         else:
             tuple_b = tuple_b[0], 0
 
-    res = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
+#    res = tuple(map(sum, zip(tuple_a, tuple_b)))
+    res = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1] 
     print(res)
