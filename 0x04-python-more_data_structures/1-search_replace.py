2e6e3835d2fd0823869d4388c1aa26768c467575
Khaikhai01 <shishchei@gmail.com>
2022-12-07 16:13:55 +0300

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    for replace, search in enumerate(new_list):
-        if search == 2:
-            new_list[replace] = 89
-    return new_list
+    replaced = list(map(lambda x: replace if x == search else x, my_list))
+    return replaced
