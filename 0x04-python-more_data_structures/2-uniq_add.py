3b6ddc6b94e99b8feed9a2c9a492f5b27abe68a0
Khaikhai01 <shishchei@gmail.com>
2022-12-07 12:38:55 +0300

    completed tasks

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = []
+    [new_list.append(x) for x in my_list if x not in new_list]
+
+    res = sum(new_list)
+    return res
---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    i
+    main_list = [item for item in set_2 if item not in set_1]
+    return main_list
