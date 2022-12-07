788016dd734968197dad6bb565174bfda6597f5a
Khaikhai01 <shishchei@gmail.com>
2022-12-08 00:57:45 +0300

    task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    added = a_dictionary.update({'city' : "San Francisco"})
-    return added
+    a_dictionary[key] = value
+    return a_dictionary
