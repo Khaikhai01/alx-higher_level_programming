276eaedfa39bdf0346d2ba76b4266cefec39bb6f
Khaikhai01 <shishchei@gmail.com>
2022-12-20 15:49:30 +0300

    corrections made

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-python3 -c 'print(__import__("my_module").__doc__)'
-python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
 class Square:
+  """python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
     """Defines an empty class Square"""
     pass
