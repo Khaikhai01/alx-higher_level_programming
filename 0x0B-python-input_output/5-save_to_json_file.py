dee4474143ef55f5eeabb1eb9fb5e68c2742e27f
Khaikhai01 <shishchei@gmail.com>
2023-01-10 21:05:44 +0300

    changed code

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,5 +7,5 @@ import json
 def save_to_json_file(my_obj, filename):
     '''opens, writes and save object to text file'''
 
-    with open(filename, 'w', encoding='utf-8') as jsFile:
+    with open(filename, 'a', encoding='utf-8') as jsFile:
         return json.dump(my_obj, jsFile)
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,4 +8,4 @@ def load_from_json_file(filename):
     '''creates an Object from a “JSON file”'''
 
     with open(filename, encoding='utf-8') as outfile:
-        return json.loads(outfile.read())
+        return json.loads(outfile)
