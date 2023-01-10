3ca81a4558123f67df35d97c713a0ec28beddf94
Khaikhai01 <shishchei@gmail.com>
2023-01-10 23:10:30 +0300

    task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''class to json dictionary'''
+import json
+
+def class_to_json(obj):
+    '''function to serialize obj'''
+    dict = {}
+    for key, value in obj.__dict__.items():
+        dict[key] = value
+    return dict
