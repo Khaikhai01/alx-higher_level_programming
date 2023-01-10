71be8d2abb038610fcfe8d809d04ef80660736ff
Khaikhai01 <shishchei@gmail.com>
2023-01-10 16:25:24 +0300

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@
 def read_file(filename=""):
     '''opening and reading contents in the file'''
 
-    with open('my_file_0.txt', encoding='utf-8') as f:
+    with open('my_file_0.txt', 'r', encoding='utf-8') as f:
         print(f.read(), end='')
